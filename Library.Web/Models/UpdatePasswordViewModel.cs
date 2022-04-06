773cc931efdab84bb78878b3b7409b6695a31920
Albaneca <48447744+Albaneca@users.noreply.github.com>
2022-04-06 16:59:50 +0300

    Add files via upload

---
+++ Library.Web/Models/UpdatePasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Library.Common;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Web.Models
+{
+    public class UpdatePasswordViewModel
+    {
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = GlobalConstants.PASSWORD_ERROR_MESSAGE)]
+        public string Password { get; set; }
+
+        [Required]
+        [DisplayName("Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = GlobalConstants.PASSWORDS_MUST_MATCH)]
+        [MinLength(8, ErrorMessage = GlobalConstants.PASSWORD_ERROR_MESSAGE)]
+        public string ConfirmPassword { get; set; }
+    }
+}
